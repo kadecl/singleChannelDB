903371afd97c96fbad346bd07a67b13dbdef70a2
kadecl <yashima777wille@gmail.com>
2025-03-01 00:31:32 +0900

    change output settings

--- Rk1NullspaceOfSylvester_STFTdomain/demoUsingOraclePhase.m
+++ Rk1NullspaceOfSylvester_STFTdomain/demoUsingOraclePhase.m
@@ -1,6 +1,6 @@
-%%% 20250228 最も基礎的なあいでぃあ．長めのリバーブに対してシングルチャネルでできているので新規性はアピールできそう．
-%%% 一方で，
-
+%%% 20250228 demo.m に，オラクル情報を使った差分
+%%% 残響除去後のスペクトルに，オラクルから得る位相差分を貼り付ける方法を検証する．位相の話がいったん落ち着くので，
+%%% 手法のメインなアイディアの良さを評価できることが期待できる．
 
 clear all
 %% settings
@@ -10,8 +10,8 @@ F = DGTtool("windowLength", 512, "windowShift", 128);
 % audio data loading
 num_piece = 2;
 for i=1:num_piece
-    audiofilename = "input/9w3r7y_01_%d.wav";
-    [temp, fs] =  audioread(sprintf(audiofilename, i));
+    audiofilename = "9w3r7y_01_%d";
+    [temp, fs] =  audioread("input/" + sprintf(audiofilename,i)+".wav");
     ss{i} = temp(:,1);
 end
 s = [ss{1}; ss{2}];
@@ -34,6 +34,7 @@ opt.solver = "c";
 rt = zeros(num_mic, 1); % reverb time in sec
 obs = cell(num_mic,num_piece); % obs
 X = cell(num_mic, num_piece); % STFTs of obs. signal
+S = cell(num_mic, num_piece); % STFTs of dry signal
 N = zeros(num_mic, num_piece); % length of obserbation
 L = zeros(num_mic, 1); % ir length = d + 1
 l = zeros(num_mic, num_piece); % length of deconvoluted signal
@@ -49,6 +50,7 @@ for i = 1:num_mic
         obs{i, j} = conv(ss{j}, ir);
         X{i, j} = F(obs{i, j});
         N(i, j) = size(X{i, j}, 2);
+        S{i, j} = F(ss{j});
     end
 end
 obsCat = [obs{1}; obs{2}];
@@ -67,8 +69,10 @@ singValRatio = zeros(Fq, 1);
 parfor f = 1:Fq
     temp = X{1, 1};
     X1f = temp(f, :)';
+    % a1 = norm(X1f); X1fとX2fの大きさで別々に正規化してたらだめじゃん．
     temp = X{1, 2};
     X2f = temp(f, :)';
+    % a2 = norm(X2f);
     Xf = [X1f; X2f];
     XfNorm = norm(Xf);
     XfPhase = Xf ./ abs(Xf);
@@ -79,15 +83,26 @@ parfor f = 1:Fq
     Sigma = diag(Sigma); singValRatio(f) = Sigma(end) / Sigma(end-1);
     Vf = V(:,end);
     Vf = XfNorm * Vf / norm(Vf); % scale recovery
-    v1 = Vf(1:l(1)); 
-    v2 = Vf(l(1)+1:end);
+    VfAng = angle(Vf);
 
-    % phase normalization
-    A = [convmtx(v1, L(1)); convmtx(v2, L(1))];
-    Hhat = A \ Xf; % calc. IR (TF domain)
-    HhatPhase = Hhat ./ abs(Hhat);
-    [Hhatmax, idx_HhatPeak] = max(abs(Hhat));
-    v1 = v1 * HhatPhase(idx_HhatPeak); v2 = v2 * HhatPhase(idx_HhatPeak); 
+    % % IR estimation
+    % A = [convmtx(v1, L(1)); convmtx(v2, L(1))];     b = [X1f; X2f];
+    % Hhat = A \ b; % calc. IR (TF domain)
+    % HhatPhase = Hhat / abs(Hhat);
+    % [Hhatmax, idx_HhatPeak] = max(abs(Hhat));
+
+    % use Oracle PHASE
+    S1f = S{1,1}(f,:); S2f = S{1,2}(f,:);  Sf = [S1f'; S2f'];
+    SfAng = angle(Sf);
+    aveAngDiff = mean(SfAng - VfAng);
+    EstPhaseUsingOracle = exp(1i * aveAngDiff);
+    Vf = Vf .* EstPhaseUsingOracle; % / Hhatmax;
+
+    % v1 = v1 / Hhatmax; v2 = v2 / Hhatmax; ::
+    % ただでさえあやしい時間周波数領域での畳み込みの成立を前提とした
+    % 誤差が蓄積しているHhatを使って正規化するのはどうなん．．ということでつかわないかも．
+    v1 = Vf(1:l(1));% v1 = a1 * v1 / norm(v1); % scale recovery
+    v2 = Vf(l(1)+1:end); %v2 = a2 * v2 / norm(v2); % scale recovery
     RET1(f,:) = v1';   RET2(f,:) = v2';
 
     % if useSLRA % && avoidNearZeroPolys
@@ -131,20 +146,25 @@ for i=1:2
     [SDR,SIR,SAR,perm] = bss_eval_sources(ret{i}(1:len_ss)', ss{i}');
     fprintf("ret: SDR %2.3f, SIR %2.3f, SAR %2.3f\n", SDR, SIR, SAR)
 
-    audiowrite(sprintf("output/wet_%d.wav", i), obs{i}, fs)
-    audiowrite(sprintf("output/lra_%d.wav", i), ret{i}, fs)
+    audiowrite("output/" + sprintf(audiofilename +"_wet_ORACLE.wav", i), obs{i}, fs)
+    audiowrite("output/" + sprintf(audiofilename +"_lra_ORACLE.wav", i), ret{i}, fs)
     % audiowrite("output/slra.wav", ret_slra, fs)
 end
 
-F.plotReassign(ss{1});title("dry"); saveas(gcf, audiofilename + "dry.png")
-F.plotReassign(obs{1}); title("wet"); saveas(gcf, audiofilename + "wet.png")
-F.plotReassign(ret{1}); title("lra"); saveas(gcf, audiofilename + "ret.png")
+F.plotReassign(ss{1});title("dry"); 
+saveas(gcf, "result/" + sprintf(audiofilename,1) + "_ORACLE_dry.png")
+F.plotReassign(obs{1}); title("wet"); 
+saveas(gcf, "result/" + sprintf(audiofilename,1) + "_ORACLE_wet.png")
+F.plotReassign(ret{1}); title("lra");
+saveas(gcf, "result/" + sprintf(audiofilename,1) + "_ORACLE_ret.png")
 if useSLRA, F.plotReassign(ret_slra); title("slra");end
 
-% obs: SDR 0.689, SIR Inf, SAR 0.689
-% ret: SDR 4.171, SIR Inf, SAR 4.171
-% obs: SDR -0.120, SIR Inf, SAR -0.120
-% ret: SDR 4.543, SIR Inf, SAR 4.543
+
 % audiowrite("output/slra.wav", ret_slra, fs)
 % player = audioplayer(obsCat, 8000, 16, 4); play(player);
-
+% 
+% obs: SDR 0.689, SIR Inf, SAR 0.689
+% ret: SDR 6.136, SIR Inf, SAR 6.136
+% obs: SDR -0.120, SIR Inf, SAR -0.120
+% ret: SDR 5.033, SIR Inf, SAR 5.033
+% 
