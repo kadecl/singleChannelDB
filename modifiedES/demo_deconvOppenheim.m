287a0ff11c743efefcb49f400edf2541a8bab7dc
kadecl <yashima777wille@gmail.com>
2025-08-07 10:53:03 +0900

    hisashiburi

--- modifiedES/demo_deconvOppenheim.m
+++ modifiedES/demo_deconvOppenheim.m
@@ -1,31 +1,11 @@
+[long_s, fs] = audioread("../data/brahms_vc_8000Hz.wav");
 [ir, fsir] = audioread("../data/BF TL SPACE LIBRARY/Drumbrella/Drumbrella 5'.L.wav");
 ir = ir(1:ceil(reverb_time(ir,fsir)*fsir));
-ir8000 = resample(ir, fs,fsir);
+ir8000 = resample(ir, fs, fsir);
 lh = length(ir8000);
 
-long_s = audioread("../data/brahms_vc_8000Hz.wav");
 y = conv(long_s, ir8000);
-
 h = ir8000;
-P = length(h);
-L = 2*P;
-Lp = P-1;
-La = L + Lp;
-
-H = fft(h, L);
-R = ceil(length(y)/(La));
-d  = zeros(L * R, 1);
-y_ext = zeros(R * La, 1);
-y_ext(1:length(y)) = y;
-
-n_range = 1:L;
-for r = 0:R-1
-    
-    yrL = y_ext(n_range + r * La);
-    YrL = fft(yrL, L);
-    dr = ifft(YrL ./ H, L);
-    d(n_range + r*L ) = dr;
-end
 
 %% convenional deconv
 % recovering 1
@@ -40,6 +20,56 @@ e = zeros(size(M,1), 1); e(1) = 1;
 h_inv = M \ e;
 z2 = conv(y, h_inv);
 
+%% oppenheim
+P = length(h); 
+L = 1024*8;  % frame length
+Q = 0;     % hamidasi
+Lp = P -1 + Q;
+La = L + Lp;
+
+% H_L = fft(h, L);
+% % 正則化と閾値処理
+% if options.adaptiveThreshold
+%     epsilon = 0.01 * max(abs(H_L));  % 動的設定
+% else
+%     epsilon = options.epsilon;       % 固定値
+% end
+% 
+% if options.regularization
+%     % 高度な正則化（Tikhonov型）
+%     H_L_safe = conj(H_L) ./ (abs(H_L).^2 + epsilon);
+% else
+%     % 単純なしきい値処理
+%     H_L_safe = H_L;
+%     H_L_safe(abs(H_L) < epsilon) = epsilon;
+% end
+% R = ceil(length(y)/(La));
+% d  = zeros(L * R, 1);
+% y_ext = zeros(R * La, 1);
+% y_ext(1:length(y)) = y;
+% 
+% n_range = 1:L;
+% for r = 0:R-1
+%     % #r frame proc.
+%     if r < R-1
+%         yhat_r = y_ext(n_range + r * La) + y_ext(n_range + r * La + L);
+%     else
+%         yhat_r = y_ext(n_range + r * La);
+%     end
+%     y_rL = yhat_r; y_rL(L+1:end) = 0;
+%     Y_rL = fft(y_rL, L);
+%     if options.regularization
+%         D_r = Y_rL .* H_L_safe;
+%     else
+%         D_r = Y_rL ./ H_L_safe;
+%     end
+%     dr = ifft(D_r, L);
+%     d(n_range + r*La ) = dr;
+% end
+
+
+d = efficient_deconvolution(y,h,L,Q,options);
+
 %% analyzing the results
 % listening
 % soundsc(y, fs); 
@@ -52,7 +82,7 @@ z2 = conv(y, h_inv);
 
 % Spectral Distance
 LogSpectralDistance(long_s, y, fs)
-LogSpectralDistance(long_s, z, fs)
+LogSpectralDistance(long_s, z1, fs)
 LogSpectralDistance(long_s, z2, fs)
 LogSpectralDistance(long_s, d, fs)
 
